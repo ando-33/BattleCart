6526e38432ba5c4c0759ca16ed5d08121da66526
DELL-ando <aguguru81@gmail.com>
2025-10-09 11:39:55 +0900

    READMEの作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -28,8 +29,15 @@ public class PlayerController : MonoBehaviour
 
     public GameObject boms;
 
+    //音にまつわるコンポーネントとSE情報
+    AudioSource audio;
+    public AudioClip se_shot;
+    public AudioClip se_damage;
+    public AudioClip se_jump;
+
     void Start()
     {
+        audio = GetComponent<AudioSource>();
         controller = GetComponent<CharacterController>();
     }
 
@@ -75,6 +83,10 @@ public class PlayerController : MonoBehaviour
         //移動後接地してたらY方向の速度はリセットする
         if (controller.isGrounded) moveDirection.y = 0;
 
+        //1秒に1ずつトップスピードの上限値が増えていく
+        speedZ += Time.deltaTime;
+
+
     }
 
     //左のレーンに移動を開始
@@ -98,7 +110,11 @@ public class PlayerController : MonoBehaviour
     {
         if (IsStun()) return;
         //地面に接触していればY方向の力を設定
-        if (controller.isGrounded) moveDirection.y = speedJump;
+        if (controller.isGrounded)
+        {
+            SEPlay(SEType.Jump);
+            moveDirection.y = speedJump;
+        }
     }
 
     //体力をリターン
@@ -129,8 +145,19 @@ public class PlayerController : MonoBehaviour
             //体力をマイナス
             life--;
 
+            SEPlay(SEType.Damage); //
+
+            //スピードをリセット
+            speedZ = 10;
+
             if (life <= 0)
             {
+                SoundManager.instance.StopBgm(); //曲を止める
+
+                //ゲームオーバーになった時にその時のポジションZの座標を
+                //Scoreキーワードでパソコンに保存
+                PlayerPrefs.SetFloat("Score", transform.position.z);
+
                 GameManager.gameState = GameState.gameover;
                 Instantiate(boms, transform.position, Quaternion.identity); //爆発エフェクトの発生
                 Destroy(gameObject, 0.5f); //少し時間差で自分を消滅
@@ -151,6 +178,23 @@ public class PlayerController : MonoBehaviour
         if (val >= 0) body.SetActive(true);
         //負の周期なら非表示
         else body.SetActive(false);
+    }
+
+    //SE再生
+    public void SEPlay(SEType type)
+    {
+        switch (type)
+        {
+            case SEType.Shot:
+                audio.PlayOneShot(se_shot);
+                break;
+            case SEType.Damage:
+                audio.PlayOneShot(se_damage);
+                break;
+            case SEType.Jump:
+                audio.PlayOneShot(se_jump);
+                break;
+        }
     }
 
 }
---
+++ README.md
@@ -0,0 +1,12 @@
+# BattleCart
+
+[ゲームのサンプルプレイ]( https://ando-33.github.io/BattleCart_web/)
+
+![ゲーム画面](readmeImg/battlecart_portfolio.jpg)
+
+
+
+
+
+
+
